3fe7ce7cd5af4858ee9dac9e162a7f7da7fe8e83
liverbool <nukboon@gmail.com>
2017-10-11 20:00:05 +0700

    add gif64

--- src/PhpMob/CmsBundle/Resources/private/js/boot.js
+++ src/PhpMob/CmsBundle/Resources/private/js/boot.js
@@ -1,4 +1,13 @@
 (function () {
+    PhpMobCms.gif64 = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
+    PhpMobCms.getApiLink = function (item, key) {
+        if (!item['_links']) {
+            return '';
+        }
+
+        return item['_links'][key]['href'];
+    };
+
     // run each time when dom insert
     PhpMobCms.runInit = function (scope) {
         $.each(PhpMobCms.inits || [], function (i, fn) {
