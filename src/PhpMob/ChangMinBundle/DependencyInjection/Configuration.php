f85cd97ce2e9006398ab492d11a79caa17ac03ce
liverbool <nukboon@gmail.com>
2017-10-30 22:11:28 +0700

    bugfix

--- src/PhpMob/ChangMinBundle/DependencyInjection/Configuration.php
+++ src/PhpMob/ChangMinBundle/DependencyInjection/Configuration.php
@@ -12,6 +12,7 @@
 namespace PhpMob\ChangMinBundle\DependencyInjection;
 
 use Sylius\Bundle\ResourceBundle\SyliusResourceBundle;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
