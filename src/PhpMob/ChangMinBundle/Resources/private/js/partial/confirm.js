a594bf255371073336e0d7d3bb509bafb0fa8529
liverbool <nukboon@gmail.com>
2017-10-30 17:14:11 +0700

    add twig sm

--- src/PhpMob/ChangMinBundle/Resources/private/js/partial/confirm.js
+++ src/PhpMob/ChangMinBundle/Resources/private/js/partial/confirm.js
@@ -28,3 +28,34 @@ $(document).on('click', '[data-delete-confirmation]', function (e) {
         }
     });
 });
+
+$(document).on('click', '[data-confirmation]', function (e) {
+    e.preventDefault();
+
+    var $el = $(this);
+
+    $.confirm({
+        theme: 'modern',
+        title: 'Confirmation',
+        icon: 'fa fa-question-circle-o',
+        content: $el.data('message') || 'Do you want to continue?',
+        buttons: {
+            confirm: {
+                text: 'Yes, I do!',
+                btnClass: 'btn-info',
+                action: function (e) {
+                    $el.addClass('disabled');
+
+                    if ($el.is('a')) {
+                        window.location.href = $el.attr('href');
+                    } else {
+                        $el.closest('form').submit();
+                    }
+                }
+            },
+            cancel: {
+                text: 'No'
+            }
+        }
+    });
+});
