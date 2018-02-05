77b7c0b67ec027a21d5c1ddbde2733afe7820edf
liverbool <nukboon@gmail.com>
2018-02-05 21:32:15 +0700

    improve css

--- src/PhpMob/CmsBundle/Resources/private/js/partial/seletize.js
+++ src/PhpMob/CmsBundle/Resources/private/js/partial/seletize.js
@@ -8,7 +8,9 @@ window['user_avatar_render'] = function (data) {
     data.item.picture = PhpMobCms.getApiLink(data.item, 'picture_70x70') || PhpMobCms['gif64'];
 
     return ('<div class="media user-option-render">'
-    + '     <div class="d-flex mr-3 avatar"><img class="img-avatar img-avatar-user" src="{item.picture}"/></div>'
+    + '     <div class="mr-3 x-avatar">'
+    + '         <img class="x-avatar__img" src="{item.picture}"/>'
+    + '     </div>'
     + '     <div class="media-body">'
     + '         <h5 class="mt-0 mb-1">{item.username}</h5>'
     + '         {item.display_name}'
