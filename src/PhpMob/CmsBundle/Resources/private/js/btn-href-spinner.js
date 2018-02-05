cdb1cf820d2a77c996b1794b6ca826b43d6f699a
liverbool <nukboon@gmail.com>
2018-02-05 19:48:56 +0700

    fix href spinner

--- src/PhpMob/CmsBundle/Resources/private/js/btn-href-spinner.js
+++ src/PhpMob/CmsBundle/Resources/private/js/btn-href-spinner.js
@@ -1,5 +1,10 @@
 $(document).on('click', 'a.btn', function (e) {
     var $btn = $(this);
+    var href = $btn.attr('href');
+
+    if ('#' === href || !href) {
+        return;
+    }
 
     $btn.spinner();
     $btn.addClass('disabled');
